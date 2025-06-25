b2627b1d0ff106e1c3f0895931e2ebea0e9d3565
EdgarSouzaGM <edgar.souza.gomes@hotmail.com>
2025-06-25 11:50:43 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto_Relatorios/Controllers/DownloadController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace Projeto_Relatorios.Controllers
+{
+    public class DownloadController : Controller
+    {
+        private readonly string _caminhoDosArquivos = @"C:\Users\edgar\Desktop\documentos";
+
+        [HttpGet]
+        public IActionResult Download(string nomeArquivo)
+        {
+            if (string.IsNullOrEmpty(nomeArquivo))
+                return BadRequest("Nome do arquivo inválido.");
+
+            var caminhoCompleto = Path.Combine(_caminhoDosArquivos, nomeArquivo);
+
+            if (!System.IO.File.Exists(caminhoCompleto))
+                return NotFound("Arquivo não encontrado.");
+
+            var conteudoArquivo = System.IO.File.ReadAllBytes(caminhoCompleto);
+            var tipoMime = "application/octet-stream"; 
+
+            return File(conteudoArquivo, tipoMime, nomeArquivo);
+        }
+    }
+}
---
+++ Projeto_Relatorios/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System;
+using System.Collections.Generic;
+
+namespace Projeto_Relatorios.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string caminhoDaPasta = @"C:\Users\edgar\Desktop\documentos";
+            List<string> listaArquivos = new List<string>();
+
+            if (Directory.Exists(caminhoDaPasta))
+            {
+                string[] arquivos = Directory.GetFiles(caminhoDaPasta);
+
+                foreach (string arquivo in arquivos)
+                {
+                    string nomeArquivo = Path.GetFileName(arquivo);
+                    listaArquivos.Add(nomeArquivo);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("A pasta especificada não existe.");
+            }
+
+            return View(listaArquivos);
+        }
+
+    }
+}
